425999011930a5e6b7ad344cf48c9439b1c5bc65
Ahmed <aessamby@gmail.com>
2025-03-20 07:19:08 +0200

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        danton: "var(--font-denton)",
+      },
+      colors: {
+        dimmed: "rgb(108, 108, 108)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
